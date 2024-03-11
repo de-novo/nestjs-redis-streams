cbf1de87f60272c058fe39cd6642fb8ea33b1f4f
Denovo <vo0v0000@naver.com>
2024-03-11 17:46:16 +0900

    test: redis stream deserializer

---
+++ tests/deserializer/redis-stream.deserializer.spec.ts
@@ -0,0 +1,33 @@
+import { RedisStreamContext } from '../../lib/context/redis-stream.context';
+import { RedisStreamResponseDeserializer } from '../../lib/redis-stream.deserializer';
+
+describe('Redis Stream Deserializer', () => {
+  const deserializer = new RedisStreamResponseDeserializer();
+  let context: RedisStreamContext;
+  beforeEach(() => {
+    context = new RedisStreamContext();
+  });
+
+  const message = [
+    'id',
+    ['headers', '{"test":"test"}', 'value', '"test-value"'],
+  ];
+
+  it('should be defined', () => {
+    expect(deserializer).toBeDefined();
+  });
+  it('should have a deserialize method', () => {
+    expect(deserializer.deserialize).toBeDefined();
+  });
+
+  describe('deserialize', () => {
+    it('should deserialize the message', () => {
+      const deserialized = deserializer.deserialize(message, context);
+      expect(deserialized).toEqual('test-value');
+    });
+    it('should set the message headers', () => {
+      deserializer.deserialize(message, context);
+      expect(context.getMessageHeaders()).toEqual({ test: 'test' });
+    });
+  });
+});
