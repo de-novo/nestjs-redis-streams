053a612cc6df8e843f0f86990db304d460d164d7
Denovo <vo0v0000@naver.com>
2024-03-11 17:30:10 +0900

    test: Redis Stream Serializer

---
+++ tests/serializer/redis-stream.serializer.spec.ts
@@ -0,0 +1,30 @@
+import { RedisStreamContext } from '../../lib/context/redis-stream.context';
+import { RedisStreamRequestSerializer } from '../../lib/redis-stream.serializer';
+
+describe('Redis Stream Serializer', () => {
+  const serializer = new RedisStreamRequestSerializer();
+  const context = new RedisStreamContext().setMessageHeaders({ test: 'test' });
+  const payload = {
+    value: 'test-value',
+    headers: {
+      test: 'test',
+    },
+  };
+
+  it('should be defined', () => {
+    expect(serializer).toBeDefined();
+  });
+  it('should have a serialize method', () => {
+    expect(serializer.serialize).toBeDefined();
+  });
+
+  it('should serialize the payload', () => {
+    const serialized = serializer.serialize(payload, context);
+    expect(serialized).toEqual([
+      'headers',
+      '{"test":"test"}',
+      'value',
+      '"test-value"',
+    ]);
+  });
+});
